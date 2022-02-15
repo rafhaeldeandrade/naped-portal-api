209f5863e92a8afb2475232f4f1930d78f42c550
Rafhael Gomes <rafhael@protonmail.com>
2022-02-15 11:18:01 -0300

    feat: users seeder

--- seeders/20220215131654-users.js
+++ seeders/20220215131654-users.js
@@ -1,3 +1,15 @@
+// Why argon2?
+// https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#argon2id
+
+const argon2 = require('argon2');
+
+const argon2Options = {
+  type: argon2.argon2id,
+  memoryCost: 2 ** 10 * 15, // 15 MB
+  timeCost: 2,
+  parallelism: 1,
+};
+
 module.exports = {
   async up(queryInterface, _Sequelize) {
     await queryInterface.bulkInsert(
@@ -6,14 +18,26 @@ module.exports = {
         {
           id: 1,
           name: 'Admin',
+          username: 'Admin',
+          password: await argon2.hash('souadmin123', argon2Options),
+          roleId: 1,
+          profileImage: null,
         },
         {
           id: 2,
           name: 'Writer',
+          username: 'Writer',
+          password: await argon2.hash('souwriter123', argon2Options),
+          roleId: 2,
+          profileImage: null,
         },
         {
           id: 3,
           name: 'Moderator',
+          username: 'Moderator',
+          password: await argon2.hash('soumod123', argon2Options),
+          roleId: 3,
+          profileImage: null,
         },
       ],
       { timestamps: false },
